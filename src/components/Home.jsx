ae7a3d03ec9661e22c40505b8e3e1c5186ee4602
juliosal16 <99932624+juliosal16@users.noreply.github.com>
2025-11-10 16:11:30 -0800

    Please work

---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import HomeBtn from './HomeBtn'
+
+const Home = () => {
+  return (
+    <div className='text-center  w-50 mx-auto'>
+      <h1 className='p-2'>Room Locator 2.0</h1>
+      <small>Powered by J-Tech</small>
+      <div className='frame mx-auto m-4'>
+        <HomeBtn unit={"P0083 CVICU"} />
+        <HomeBtn unit={"P0080 MRI"} />
+        <HomeBtn unit={"P0081 MRI"} />
+        <HomeBtn unit={"P0085 MOTHER CART"} />
+        <HomeBtn unit={"P1209 BASS"} />
+        <HomeBtn unit={"P0084 2F PPF"} />
+        <HomeBtn unit={"P0082 PCU"} />
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/HomeBtn.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const HomeBtn = ({ unit }) => {
+  return (
+    <button type="button" className="btn btn-outline-info w-75 d-block mx-auto m-3">{unit}</button>
+
+  )
+}
+
+export default HomeBtn
